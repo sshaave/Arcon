fe417046e5d2188fc2055e53fc9416be8669b5c1
simensh <sshaave@gmail.com>
2019-03-06 21:56:40 +0100

    changed qSort.py

--- q/qSort.py
+++ q/qSort.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
 from operator import itemgetter
 import re
+import os
+import sys
+# 1st input: name of connection file from getGNO
+# 2nd input: name of 2nd connection file
+# 3rd input: name of output file
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+file3 = sys.argv[3]
+with open(file3,'w') as f3:
+    with open(file1,'r') as f1:
+        for line in f1:
+	    f3.write(line)
+	with open(file2,'r') as f2:
+	    for line in f2:
+	        f3.write(line)
+
 def f1(text):
     result = re.search('master="(.*)" mface',text)
     return int(result.group(1))
 
-
-
-with open("q1.txt","r") as file:
+with open(file3,"r") as file:
     l = file.read()
-#    print(l)
     l2=l.splitlines()
-#    print(l2)
     l2.sort(key=f1)
-#    print(l2)
-    for x in l2:
-        print(x)
-#       print('\n')
-#with open("y1.txt","r") as readfile:
-#    types = (line.split(">") for line in readfile)
-#    xys = ((type[1], type[2]) for type in types)
-#    for x,y in xys:
-#        print(x,y)
+    with open('con.txt','r') as con:
+        for x in l2:
+            con.write(x)
+
